1fb20f3334ba13b9e93473fe71ddba3a1e9ea7b0
Swapnil Bankar <swapnil@etgresearch.in>
2022-09-28 17:01:26 +0530

    hh

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,29 +1,29 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "password123",
-    DB: "mbtest",
-    dialect: "mysql",
-    table:'test',
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
-
 // module.exports = {
-//   HOST: "68.178.228.104",
-//   USER: "swapnil12",
-//   PASSWORD: "swapnil12",
-//   DB: "idingdata",
-//   dialect: "mysql",
-//   port:"3306",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// };
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "password123",
+//     DB: "mbtest",
+//     dialect: "mysql",
+//     table:'test',
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+//   };
+
+module.exports = {
+  HOST: "68.178.228.104",
+  USER: "swapnil12",
+  PASSWORD: "swapnil12",
+  DB: "idingdata",
+  dialect: "mysql",
+  port:"3306",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
